f1af39a0d8b2034d06b497e3a802e6646cccaa19
Alexander Skiba <alexander.skiba@timetac.com>
2020-07-29 12:24:47 +0200

    bump navicat-for-mysql version to 12.1.30

--- Casks/navicat-for-mysql@12.rb
+++ Casks/navicat-for-mysql@12.rb
@@ -1,6 +1,6 @@
 cask 'navicat-for-mysql@12' do
-  version '12.1.27'
-  sha256 'ea969a9d837eaf4d79df3df736fdbd70a273ef29e50a58c27b804d940711c28f'
+  version '12.1.30'
+  sha256 '4687d518db632352cd5bfd2832451a0716be4f4d5528e50f8b70760dd2e9334f'
 
   url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_mysql_en.dmg"
   appcast 'https://www.navicat.com/updater/v120/sysProfileInfo.php?appName=Navicat%20for%20MySQL&appLang=en'

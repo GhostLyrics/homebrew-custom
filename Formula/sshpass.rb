a15042eb9ee98acb466759e7553cddb8bd1809e4
Alexander Skiba <alexander.skiba@timetac.com>
2020-08-10 23:39:08 +0200

    Formula: separate install commands for sshpass
    Co-Authored-By: twu <twu@users.noreply.github.com>

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   desc "SSH passwort automation. Use only for edge-cases with Ansible secrets"
   homepage "https://sourceforge.net/projects/sshpass"
-  url "https://downloads.sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
   sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
 
   depends_on "gnu-sed"

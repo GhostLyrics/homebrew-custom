05e94b428a33bf55006bcb662b04c084e7a2c150
Alexander Skiba <alexander.skiba@timetac.com>
2021-11-18 13:49:33 +0100

    Fix some brew audit issues

--- Casks/ttvpn.rb
+++ Casks/ttvpn.rb
@@ -2,11 +2,10 @@ cask "ttvpn" do
   version "1.1.1"
   sha256 "61e66fa3889091c33978c4258a74d5dfccfd441ef5d17604f77d2d931019e14a"
 
-  url "https://static.timetac.com/ttvpn/ttvpn-darwin-amd64",
-      verified: "static.timetac.com"
+  url "https://static.timetac.com/ttvpn/ttvpn-darwin-amd64"
   name "ttvpn"
   desc "TLS based, secure, punch-through VPN solution"
-  homepage "https://www.timetac.com"
+  homepage "https://www.timetac.com/"
 
   binary "ttvpn-darwin-amd64",
          target: "ttvpn"
